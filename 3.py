1f2491b1bd5d9f453a0eeaf9e5bed7e459098084
evⁿ <gh@cli.lol>
2022-11-08 10:23:30 +0100

    CONT. 4.py

--- 3.py
+++ 3.py
@@ -1,7 +1,10 @@
 tour = 0
 
+def JouerUnTour():
+    return 1
+
 # Tant que je ne suis pas au tour 5
-while tour < 5
+while tour < 5:
     # Appeler la fonction JouerUnTour
     JouerUnTour()
     # Puis passer le tour
--- 4.py
+++ 4.py
@@ -4,13 +4,27 @@ Exercice:
     Le charactere doit être parametrable
 """
 
-character = 'x'
+"""
+import random
 
-# Pendant que input renvoye quelquechose different de character
-# Si l'input et égal a character, la boucle sera finie
-while input("Devine le caractère secret...> ") != character:
-    # Afficher "Mauvaise réponse!"
-    print("Mauvaise réponse!")
+# Definir l'alphabet
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+# Puis choisir une lettre aléatoirement
+letter = random.choice(alphabet)
+"""
+
+# Definir la lettre secrette
+letter = "x"
+
+tries = 0
+
+# Pendant que input renvoye quelquechose different de letter
+# Si l'input et égal a letter, la boucle sera finie
+while input("Devine la lettre secrette...> ").lower() != letter.lower():
+    # Incrémenter le nombre d'essais...
+    tries = tries + 1
+    # Afficher "Mauvaise réponse! <tries>"
+    print(f"Mauvaise réponse! Tu es a {tries} essais.")
 
 # Afficher "Bien joué!" une fois la boucle finie
 print("Bien joué!")
